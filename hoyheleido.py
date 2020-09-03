64ecbac7399ad2a7dd52cda2448014d91b367ec0
moop <moop@pop-os.localdomain>
2020-09-03 20:12:19 +0200

    added keys and .gitignore

---
+++ hoyheleido.py
@@ -0,0 +1,13 @@
+import tweepy
+from keys import *
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+    print(str(mention.id) + " - " mention.text)
